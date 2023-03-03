bac8bf35d5bfdf4cfe0393e7b2ab02d197ae66bc
arthurtsenin <arturcenin@gmail.com>
2023-03-03 20:25:59 +0300

    feat(SideMenuItem):create item for side menu

---
+++ src/components/views/SideMenuItem/SideMenuItem.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+
+import { ISideMenuItem } from './SideMenuItem.interfaces'
+
+export const SideMenuItem: FC<ISideMenuItem> = ({ onClick, Icon, text }) => {
+  return (
+    <ListItem onClick={onClick} key={text} disablePadding>
+      <ListItemButton component={Link} to={`/${text}`}>
+        <ListItemIcon>
+          <Icon />
+        </ListItemIcon>
+        <ListItemText>{text}</ListItemText>
+      </ListItemButton>
+    </ListItem>
+  )
+}
