7bf916c9e29f8f516f07a157883d6fdfc74e7ea5
arthurtsenin <arturcenin@gmail.com>
2023-03-21 10:39:00 +0300

    fix(UserAvatar):fix imports,remove Box

--- src/components/views/UserAvatar/UserAvatar.tsx
+++ src/components/views/UserAvatar/UserAvatar.tsx
@@ -1,19 +1,14 @@
 import { FC } from 'react'
-import Avatar from '@mui/material/Avatar'
-import Box from '@mui/material/Box'
-import IconButton from '@mui/material/IconButton'
-import Tooltip from '@mui/material/Tooltip'
+import { Avatar, IconButton, Tooltip } from '@mui/material'
 
 import { IUserAvatarProps } from './UserAvatar.interfaces'
 
 export const UserAvatar: FC<IUserAvatarProps> = ({ handleClick }) => {
   return (
-    <Box>
-      <Tooltip title="User account ">
-        <IconButton onClick={handleClick} sx={{ ml: 2 }}>
-          <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-        </IconButton>
-      </Tooltip>
-    </Box>
+    <Tooltip title="User account ">
+      <IconButton onClick={handleClick} sx={{ ml: 2 }}>
+        <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+      </IconButton>
+    </Tooltip>
   )
 }
