b196a1f2397d4df4ff4abaffade7838b7f60dd1e
arthurtsenin <arturcenin@gmail.com>
2023-04-01 17:11:58 +0300

    feat(EmployeeSkillsProfileForm): add ModalWindow,

---
+++ src/components/views/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import ClearIcon from '@mui/icons-material/Clear'
+import { DialogContent, IconButton } from '@mui/material'
+
+import { IModalWindowProps } from './ModalWindow.interfaces'
+import { Dialog } from './ModalWindow.styles'
+
+export const ModalWindow: FC<IModalWindowProps> = ({ children, modalOpen, closeModal }) => {
+  return (
+    <>
+      <Dialog
+        sx={{ display: 'flex', flexDirection: 'column' }}
+        onClose={closeModal}
+        open={modalOpen}
+      >
+        <IconButton sx={{ alignSelf: 'flex-end' }} onClick={closeModal}>
+          <ClearIcon />
+        </IconButton>
+        <DialogContent>{children}</DialogContent>
+      </Dialog>
+    </>
+  )
+}
