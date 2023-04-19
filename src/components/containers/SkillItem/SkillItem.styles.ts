5e7a89fc3e23f30f6c8efbd912609fbe4aff8402
KleimyonovVladimir <kleimionovvova@gmail.com>
2023-04-19 21:22:25 +0300

    feat(): change styles in skills employee page

--- src/components/containers/SkillItem/SkillItem.styles.ts
+++ src/components/containers/SkillItem/SkillItem.styles.ts
@@ -3,24 +3,25 @@ import { Box, styled } from '@mui/material'
 import { IBadgeStyled } from './SkillItem.interfaces'
 
 export const SkillItemContainer = styled(Box)(() => ({
-  margin: '5px',
-  display: 'inline-flex',
-  alignItems: 'center'
+  padding: '16px',
+  display: 'flex',
+  alignItems: 'center',
+  gridColumnGap: '20px',
+  justifyContent: 'space-between'
 }))
 
 export const SkillBox = styled(Box)(({ theme }) => ({
-  display: 'inline-flex',
+  display: 'flex',
   alignItems: 'center',
+  justifyContent: 'space-between',
   borderRadius: '20px',
-  color: 'white',
-  padding: '5px',
-  backgroundColor: theme.palette.secondary.main
+  color: theme.palette.secondary.main,
+  minWidth: '250px'
 }))
 
-export const MasteryBox = styled(Box)<IBadgeStyled>(({ theme, mastery_color }) => ({
+export const MasteryBox = styled(Box)<IBadgeStyled>(({ mastery_color }) => ({
   backgroundColor: mastery_color,
-  color: theme.palette.secondary.main,
-  borderRadius: '15px',
-  padding: '5px',
-  marginLeft: '5px'
+  color: '#fff',
+  borderRadius: '16px',
+  padding: '5px 14px'
 }))
